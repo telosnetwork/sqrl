23a21d52389735ddb50123c8a65f8a248a1e8656
Marlon Williams <marlonjwilliams@gmail.com>
2019-07-02 16:13:20 -0400

    Support for WAX blockchain

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-amd64.deb"
-shasum -b -a 512 linux-Sqrl-1.0.8-amd64.deb
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-amd64.snap"
-shasum -b -a 512 linux-Sqrl-1.0.8-amd64.snap
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-arm64.deb"
-shasum -b -a 512 linux-Sqrl-1.0.8-arm64.deb
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-armv7l.deb"
-shasum -b -a 512 linux-Sqrl-1.0.8-armv7l.deb
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-i386.deb"
-shasum -b -a 512 linux-Sqrl-1.0.8-i386.deb
-echo "shasum -b -a 512 linux-Sqrl-1.0.8-x86_64.AppImage"
-shasum -b -a 512 linux-Sqrl-1.0.8-x86_64.AppImage
-echo "shasum -b -a 512 mac-Sqrl-1.0.8.dmg"
-shasum -b -a 512 mac-Sqrl-1.0.8.dmg
-echo "shasum -b -a 512 mac-Sqrl-1.0.8.zip"
-shasum -b -a 512 mac-Sqrl-1.0.8.zip
-echo "shasum -b -a 512 win-Sqrl-1.0.8.exe"
-shasum -b -a 512 win-Sqrl-1.0.8.exe
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-amd64.deb"
+shasum -b -a 512 linux-Sqrl-1.0.9-amd64.deb
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-amd64.snap"
+shasum -b -a 512 linux-Sqrl-1.0.9-amd64.snap
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-arm64.deb"
+shasum -b -a 512 linux-Sqrl-1.0.9-arm64.deb
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-armv7l.deb"
+shasum -b -a 512 linux-Sqrl-1.0.9-armv7l.deb
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-i386.deb"
+shasum -b -a 512 linux-Sqrl-1.0.9-i386.deb
+echo "shasum -b -a 512 linux-Sqrl-1.0.9-x86_64.AppImage"
+shasum -b -a 512 linux-Sqrl-1.0.9-x86_64.AppImage
+echo "shasum -b -a 512 mac-Sqrl-1.0.9.dmg"
+shasum -b -a 512 mac-Sqrl-1.0.9.dmg
+echo "shasum -b -a 512 mac-Sqrl-1.0.9.zip"
+shasum -b -a 512 mac-Sqrl-1.0.9.zip
+echo "shasum -b -a 512 win-Sqrl-1.0.9.exe"
+shasum -b -a 512 win-Sqrl-1.0.9.exe
 
 cd ..
